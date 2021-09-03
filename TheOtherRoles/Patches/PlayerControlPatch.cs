be4892e21580eb1450076e8079a96a9f8361db22
fourthoneout <toma.onto@gmail.com>
2021-09-03 23:08:21 +0900

    Implement Madmate.impostorsCanKillAnyone

--- TheOtherRoles/Patches/PlayerControlPatch.cs
+++ TheOtherRoles/Patches/PlayerControlPatch.cs
@@ -173,6 +173,12 @@ namespace TheOtherRoles.Patches {
                 } else {
                     target = setTarget(true, true, new List<PlayerControl>() { Spy.spy });
                 }
+            } else if (Madmate.madmate != null) {
+                if (Madmate.impostorsCanKillAnyone) {
+                    target = setTarget(false, true);
+                } else {
+                    target = setTarget(true, true, new List<PlayerControl>() { Madmate.madmate });
+                }
             } else {
                 target = setTarget(true, true);
             }
