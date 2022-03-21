fe9d99692130d3b92411f450f195680b19b2d404
Haoming <nh.haoming@gmail.com>
2022-03-21 19:05:35 +0900

    v1.11.17

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -22,7 +22,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
 
-        public const string VersionString = "1.11.16";
+        public const string VersionString = "1.11.17";
         
         public static System.Version Version = System.Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
