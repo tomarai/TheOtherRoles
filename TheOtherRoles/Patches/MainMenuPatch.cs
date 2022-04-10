f3560e487dc62e7db2e4f0a5dd260a4013f04dd2
fourthoneout <toma.onto@gmail.com>
2022-04-10 11:27:18 +0900

    Remove Discord link
    Remove the discord button linking to the TheOtherRoles discord server to
    avoid users asking other developers about bugs related to **this** mod.

--- TheOtherRoles/Patches/MainMenuPatch.cs
+++ TheOtherRoles/Patches/MainMenuPatch.cs
@@ -17,29 +17,6 @@ namespace TheOtherRoles.Modules {
             var template = GameObject.Find("ExitGameButton");
             if (template == null) return;
 
-            var buttonDiscord = UnityEngine.Object.Instantiate(template, null);
-            buttonDiscord.transform.localPosition = new Vector3(buttonDiscord.transform.localPosition.x, buttonDiscord.transform.localPosition.y + 0.6f, buttonDiscord.transform.localPosition.z);
-
-            var textDiscord = buttonDiscord.transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
-            __instance.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) => {
-                textDiscord.SetText("Discord");
-            })));
-
-            PassiveButton passiveButtonDiscord = buttonDiscord.GetComponent<PassiveButton>();
-            SpriteRenderer buttonSpriteDiscord = buttonDiscord.GetComponent<SpriteRenderer>();
-
-            passiveButtonDiscord.OnClick = new Button.ButtonClickedEvent();
-            passiveButtonDiscord.OnClick.AddListener((UnityEngine.Events.UnityAction)delegate {
-                Application.OpenURL("https://discord.gg/77RkMJHWsM");
-            });
-
-            Color discordColor = new Color32(88, 101, 242, byte.MaxValue);
-            buttonSpriteDiscord.color = textDiscord.color = discordColor;
-            passiveButtonDiscord.OnMouseOut.AddListener((UnityEngine.Events.UnityAction)delegate {
-                buttonSpriteDiscord.color = textDiscord.color = discordColor;
-            });
-
-
             // Horse mode stuff
             var horseModeSelectionBehavior = new ClientOptionsPatch.SelectionBehaviour("Enable Horse Mode", () => MapOptions.enableHorseMode = TheOtherRolesPlugin.EnableHorseMode.Value = !TheOtherRolesPlugin.EnableHorseMode.Value, TheOtherRolesPlugin.EnableHorseMode.Value);
 
